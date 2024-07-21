06b1dda02a3249518ee23823ca433fa58186b578
BonyChops <contact@bonychops.com>
2024-07-21 20:02:05 +0900

    flask inertia setup

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_vite import Vite
+from flask_inertia import Inertia
+from flask_inertia import render_inertia
+
+SECRET_KEY = "secret!" # 適宜変えてください
+INERTIA_TEMPLATE = "base.html"
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+vite = Vite(app)
 
+inertia = Inertia()
+inertia.init_app(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    data = {}
+    return render_inertia(
+        component_name="Index",
+        props=data,
+        view_data={},
+    )
 
+@app.route('/_vite-static/<path:path>')
+def send_public(path):
+    return send_from_directory('vite/dist', path)
 
 if __name__ == '__main__':
     app.run()
